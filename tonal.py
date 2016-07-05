4ecb8c9a8800a4daf68cd267706fccf55bbe27e5
Gabriel Levine <gabriel@magnetic.com>
2016-07-05 13:23:32 -0400

    fixing things

--- tonal.py
+++ tonal.py
@@ -11,5 +11,15 @@ for x in range(0, 100):
 
 import mido
 import time
+import random
 
+output = mido.open_output()
 
+for x in range(0, 1000):
+    note=random.randint(0, 120)
+    vel=random.randint(40,100)
+    output.send(mido.Message('note_on', note=note, velocity=vel))
+    print(note, vel)
+    time.sleep(4)
+    output.send(mido.Message('note_off', note=note, velocity=vel))
+    time.sleep(1)
