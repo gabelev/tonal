8809eae2556d2501110c9cfdca6ce5209f97f96f
Gabriel Levine <gabriel@magnetic.com>
2016-07-10 15:58:25 -0400

    adding classes and tests

--- tonal.py
+++ tonal.py
@@ -1,7 +1,9 @@
 """
 Tell me what you are.
 """
+import random
 
+from scales import scale_names
 import mingus.core.notes
 import mingus.core.scales
 
@@ -16,8 +18,8 @@ base_scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 
 class Tonal(object):
-    def __init__(self):
-        pass
+    def __init__(self, scale=None):
+        self.scale = scale
 
     def note_to_int(self, note):
         if not isinstance(note, str) or len(note) != 1:
@@ -25,12 +27,21 @@ class Tonal(object):
         else:
             return notes_map.get(note)
 
-    def scale(self, note_int, scale):
+    def add_octave(self, note_int, scale):
         return note_int + scale * 12
 
     # This should either be random or determined. If random let's weight to the "normal" scales.
     # List of scales to be "seeds" to be used to generarate full note range.
-    def pick_scale(self):
+    def pick_scale(self, scale=None):
+        if not scale:
+            return scale_names[random.randint(0, len(scale_names)-1)]
+        else:
+            return scale
+
+    def pick_base_note(self):
+        return notes.int_to_note(random.randint(0, 11))
+
+    def create_scale_object(self):
         pass
 
     # takes a list of notes from scale and builds a [0-120] integer list that we can use to map to.
